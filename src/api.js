9217b1ae225aae0f7a6478857c0d4c92c1fa861d
Gurusaran V R <guruzaran@gmail.com>
2024-01-17 15:13:13 -0500

    Initial commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '13c3b71d3amshba3647d523cccfcp1e2888jsn5a6b3c56aaf1',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+
+export const WEATHER_API_KEY = "766926b0bb2a5f8d30e11db3954f88d0";
+
